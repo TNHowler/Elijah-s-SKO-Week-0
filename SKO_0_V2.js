e96e254fb823ac6f8abfd015c1103ae7c67306fc
Elijah <s3945008@student.rmit.edu.au>
2024-10-25 23:19:48 +1100

    Add files via upload

--- SKO_0_V2.js
+++ SKO_0_V2.js
@@ -1,28 +1,34 @@
+///GitHub Copilot assisted in writing this code.
+
 var elevator;
 var ellipses = [];
+let InterstateBCFont;
+var cheeseburger;
 
 function preload(){
   elevator = loadImage("data/Elevator.jpeg");
+  InterstateBCFont = loadFont("Interstate_BoldCondensed.ttf");
+  cheeseburger = loadImage("data/cheeseburger.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipses = [
     { x: width / 1.1321, y: height / 1.475, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-CW/" },
-    { x: width / 1.1321, y: height / 1.617, w: width / 40, h: height / 22, url: "https://example.com/2" },
-    { x: width / 1.1321, y: height / 1.789, w: width / 40, h: height / 22, url: "https://example.com/3" },
-    { x: width / 1.1321, y: height / 2, w: width / 40, h: height / 22, url: "https://example.com/4" },
-    { x: width / 1.1321, y: height / 2.27, w: width / 40, h: height / 22, url: "https://example.com/5" },
-    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/6" },
-    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://example.com/7" },
+    { x: width / 1.1321, y: height / 1.617, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-1/" },
+    { x: width / 1.1321, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-3/" },
+    { x: width / 1.1321, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-5/" },
+    { x: width / 1.1321, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-7/" },
+    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-9/" },
+    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-11/" },
 
-    { x: width / 1.0925, y: height / 1.475, w: width / 40, h: height / 22, url: "https://example.com/8" },
-    { x: width / 1.0925, y: height / 1.617, w: width / 40, h: height / 22, url: "https://example.com/9" },
-    { x: width / 1.0925, y: height / 1.789, w: width / 40, h: height / 22, url: "https://example.com/10" },
-    { x: width / 1.0925, y: height / 2, w: width / 40, h: height / 22, url: "https://example.com/11" },
-    { x: width / 1.0925, y: height / 2.27, w: width / 40, h: height / 22, url: "https://example.com/12" },
-    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/13" },
-    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/The-Zipped-Face-Final/" }
+    { x: width / 1.0925, y: height / 1.475, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-0/" },
+    { x: width / 1.0925, y: height / 1.617, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-2/" },
+    { x: width / 1.0925, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-4/" },
+    { x: width / 1.0925, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-6/" },
+    { x: width / 1.0925, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-8/" },
+    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-10/" },
+    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-12/" },
   ];
 }
 
@@ -30,21 +36,53 @@ function draw() {
   background("white");
   image(elevator, 0, 0, windowWidth, windowHeight);
 
+  imageMode(CENTER);
+  image(cheeseburger, width / 2, height / 2.25, width / 10, height / 5);
+
+  imageMode(CORNER);
+
   strokeWeight(width / 550);
   noFill();
 
   for (let i = 0; i < ellipses.length; i++) {
     let e = ellipses[i];
-    if (i === 0) {
+    if (i === 7) {
       stroke(0, 255, 0);
-    } else if (i === 1) {
-      stroke(255, 0, 0);
     } else {
       stroke(255, 0, 0);
     }
     ellipse(e.x, e.y, e.w, e.h);
   }
-}
+
+
+  // Display text in the middle of the screen
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 50);
+  textFont(InterstateBCFont); // Apply the custom font
+  noStroke();
+  fill(0); // Set text color to black
+  text(
+    "Codewords Creative Coding and Typography",
+    width / 3,
+    height / 10,
+    width / 3, // Specify the width for wrapping the text
+    height / 12,  // Specify the height for wrapping the text
+  );
+
+  // Display text in the middle of the screen
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 75);
+  textFont(InterstateBCFont); // Apply the custom font
+  noStroke();
+  fill(0); // Set text color to black
+  text(
+    "Unfortunately week 0 was uneventful... I had not checked my emails in the morning and found out that the class was canceled, but at least I met Kai and Kevin. Before leaving for home I got a triple cheeseburger meal from Macca's. I'm a bit of a fat ass, I know.",
+    width / 2.65,
+    height / 2,
+    width / 4, // Specify the width for wrapping the text
+    height / 2  // Specify the height for wrapping the text
+  );
+};
 
 function mousePressed() {
   for (let i = 0; i < ellipses.length; i++) {
